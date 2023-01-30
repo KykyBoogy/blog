d9151afc0ce21eba7559fd5bc5419e3eee7cde28
KykyBoogy <fmoeuf@gmail.com>
2023-01-30 13:03:47 +0100

    Databases Symfony

---
+++ src/DataFixtures/ArticleFixtures.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Article;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class ArticleFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        for($i = 1; $i <= 10; $i++) {
+
+            $article = new Article();
+            $article->setTitre("Article n°".$i);
+            $article->setContenu("Ceci est le contenu de l'article");
+
+            $date = new \DateTime();
+            $date->modify('-'.$i.' days');
+
+            $article->setDateCreation($date);
+
+            $manager->persist($article);
+
+        }
+
+        $manager->flush();
+    }
+}
